4effd2a7cdae84d746e761d38dc4eb9fb6134a1a
Muzaffar Ahmad <maahi@Muzaffars-MacBook-Air.local>
2022-12-27 17:18:20 +0530

    React Js Initial tutorial Class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Menu from './components/menu'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu />
         <p>
           Hi Folks! we have started our internship learning project
         </p>
---
+++ src/components/menu.js
@@ -0,0 +1,25 @@
+import  './menu.css'
+
+function Menu() {
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a>Home1</a>
+                    </li>
+                    <li>
+                        <a>About1</a>
+                    </li>
+                    <li>
+                        <a>Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Menu;
+
+
