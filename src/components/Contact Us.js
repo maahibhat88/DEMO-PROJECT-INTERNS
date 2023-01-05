ae8ce123a28bea35b077f75b176472d1816b1dac
rasheeqa nabi <nabirasheeqa@gmail.com>
2023-01-05 18:18:49 +0530

    bootstrap

--- src/components/Contact Us.js
+++ src/components/Contact Us.js
@@ -3,12 +3,42 @@ import React from "react";
 function Contact(){
     return (
         <div>
+            <div className="container">
+              <div className="row">
+                <div className="col 6 mt-4 pt-5 ">
+                    <h1>CONTACT US</h1>
             
-                <h1>
-                   Welcome to  my Contact page 
-                </h1>
+                    <h1 className="pt-6 mt-5">Call  Us</h1>
+                    <h4>+91 990645789</h4>
+                
+                    <h1 className="pt-6 mt-4">Location</h1>
+                    <h4>12 Iqbal Lane Allouchi Bagh 190009</h4>
+                
+                    <h1 className="pt-6 mt-4">Business Hours</h1>
+                    <h4>Mon-Fri --10am to 6pm, Sat-Sun --Closed</h4>
+                    
+                    </div>
+
+                <div className="col 6 mt-4 pt-5">
+                <input type="text" class="form-control form-control-lg p-3 m-4" placeholder="Name"/>
+                <input type="text" class="form-control form-control-lg p-3 m-4" placeholder="Email"/>
+                <textarea  class="form-control form-control rows=5 p-3 m-4" placeholder="Message"/>
+                <button type="submit" class="btn btn-success btn-lg mx-5">Submit</button>   
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-facebook-f"></i>
+                  </button>          
+                </div>
+
+             
+            </div>
+          </div>
+    </div>
+            
+
+            
+                
                
-        </div>
+      
     );
 }
 export default Contact;
--- src/components/home.js
+++ src/components/home.js
@@ -11,7 +11,7 @@ function Home(){
              <h3 className="text-center text-muted mt-4"> Preventing exploitation through ethical and responsive tourism.
             </h3> 
             <h3 className="text-center text-muted"> Stopping Human Exploitation by creating jobs.</h3>
-            <p className="btn btn-success btn-lg btn-get-started mt-4">Find your adventure</p>
+            <p className="btn btn-success btn-lg btn-get-started mt-5 mx-5 justify-content">Find your adventure</p>
          </div>
          <div className="col-4  bg-success text-muted mt-5 pt-3">
          <img src='https://cdn5.vectorstock.com/i/1000x1000/84/34/travel-concept-traveling-background-vector-20328434.jpg' className='img-fluid'  alt='alter' />
