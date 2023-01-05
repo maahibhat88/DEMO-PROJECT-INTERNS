7743f97344a556f265b8e191c05561407f3c5ed7
irtiqayousuf <irtiqayousuf786@gmail.com>
2023-01-05 21:38:58 +0530

    additional work of page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,39 @@
+import { Fragment } from "react";
+
 function Contact(){
     return(
-        <h1> Contact</h1>
-    );
+        <>
+         <div className="container mt-4 scrollto">
+             <div className="row">
+               <div className="col-6" >
+               <div className="hero-container "><h3 className="text-success">Contact Info</h3>
+                      <hr/>
+
+                      <div className="row">
+                        <strong>Address:</strong><br className="row"/>
+                        <dd className="list-inline-item ">Jawahar Nagar Srinagar kashmir.</dd>
+                        <dd className="list-inline-item ">Pin-Code : 190008</dd>
+                        <dd className="list-inline-item ">Landmark: Opposite JK Bank</dd>
+                     </div>
+<br/>
+                     <strong className="text-success">Write feedback:</strong>
+                     <textarea  class="form-control form-control rows=5  " placeholder="Message"/>
+                     <button type="submit" class="btn btn-success mx-0 my-3">Send</button>
+                 </div>     
+                </div>
+                <div className="col-6">
+                <div className="hero-container "><h3 className="text-success">Map Info</h3>
+                      <hr/>
+                      <img src='https://th.bing.com/th/id/R.77cf1f6c8a59750ea178f235acb89e3e?rik=YwpGmJQa0sAYVA&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f06%2fWorld-map-wallpaper-HD-download.jpg&ehk=7qmmkw6P%2faXKcvQerDzoxwRpzh%2flqHBtSV4J%2bJpKSE0%3d&risl=&pid=ImgRaw&r=0' className="rounded mx-auto img-fluid"/>
+                      </div>
+                    
+                </div>
+              </div>      
+
+
+
+           </div>     
+       </>
+   );
 }
 export default Contact;
--- src/pages/home.js
+++ src/pages/home.js
@@ -22,7 +22,7 @@ function Home(){
                <p className="hero-container ">
                <h1 className="text-white text-center p-3 ">Call To Action</h1>
                <p className="text-center text-white ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-               <p className="btn btn-success btn-lg btn-get-started mt-4 rounded p-9 border-white
+               <p className="btn btn-success btn-lg btn-get-started mt-4 rounded p-9 
                d-flex align-items-center justify-content-center">Call</p>
                </p>
             </div>                 
