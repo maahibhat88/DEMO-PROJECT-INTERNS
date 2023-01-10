369ce8e951a79b3b5f480025e855dc936b1d3732
InuKhan <inushakhan123@gmail.com>
2023-01-10 09:57:26 +0530

    travel site

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
  import './footer.css';
+ import { Link, NavLink } from 'react-router-dom';
  
 function Footer(){
     return(
@@ -6,11 +7,38 @@ function Footer(){
 <section class="container-fluid footer text-center text-black">
   <div class="mb-4">
   <div class="container p-4">
-      <h5>
-       E-Authentication Stystem |  © 2023 Copyright:
+     
+      <div class='row text-left line'>
+      <div class='col-4 text-left'><p><b>Kashmir Travels</b></p>
+      <p><b>Contact: 992929292909</b></p>
+      </div>
+
+      <div class='col-4 '>
+      <Link class=' text-dark text-bold' to='/App'><b class='bullet'>Home</b></Link><br/>
+      <Link class=' text-dark' to='/about'><b>About</b></Link><br/>
+      <Link class=' text-dark' to='/places'><b>Places</b></Link>
+      </div>
+
+      <div class='col-4 '>
+      <form>
+    <div class="mb-3 mt-3">
+     <h5>Any Queries</h5>
+      <textarea class="form-control" rows="1" id="comment" name="text"></textarea>
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+  </form>
+      </div>
+      </div>
+  </div>
+
+  <div class='container-fluid pt-4 pb-4 bg-secondary'>
+  <h5>
+       Kashmir Travels |  © 2023 Copyright:
       </h5>
   </div>
+
   </div>
+ 
 </section>
 </>
 );
