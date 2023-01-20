0d729fa04065257d1a748a512bc39b7598e480dd
Muzaffar Ahmad <maahi@Muzaffars-MacBook-Air.local>
2023-01-20 17:38:16 +0530

    Registration

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,44 @@
 import './App.css';
 import axios from "axios";
 
-const seeValue = (event) => { 
-  console.log("event ",event);
-  document.getElementById("demo").innerText = event.target.value;
-}
-
 
 function App() {
 
   const formHandler = (e) => {
     e.preventDefault();
-    console.log("e",e.target[0].value);
-    let username = e.target[0].value;
+    console.log("e",e);
+    let name = e.target[0].value;
+    let email = e.target[1].value;
+    let password = e.target[2].value;
 
     // $.post("url",data,(out)=>{
 
     // })
 
     let config = { 'headers' : { 'Content-Type' : 'application/json' } };
-    const response = axios.post("http://localhost:8080/users",{ username : username });
+    const response = axios.post("http://localhost:8080/createUser",{ name : name, email : email , password : password });
     console.log("Response ",response);
   }
 
   return (
     <>
-        <h1>We are on the home page</h1>
-        <p>
+        <h1>We are on the Login Page</h1>
+        {/* <p>
           Hi Folks! we have started our internship learning project
-        </p>
+        </p> */}
         {/* <form action="http://localhost:8080/users" method="post"> */}
         <form onSubmit={(e)=>{ formHandler(e) }}>
           <div>
-            <input onChange={seeValue}  name="username" type="text" placeholder='Enter First Name' />  <br/>
-            <span>We will see the input value here : <span id="demo"></span> </span>
+            <input name="name" type="text" placeholder='Enter Name' />  <br/>
           </div>
           <div>
-            <button type="submit">Click to see Get in Action</button>
+            <input name="email" type="text" placeholder='Enter Email' />  <br/>
+          </div>
+          <div>
+            <input name="password" type="text" placeholder='Enter Password' />  <br/>
+          </div>
+          <div>
+            <button type="submit">Register</button>
           </div>
 
           {/* localhost:3000?firstName=Muzaffar */}
